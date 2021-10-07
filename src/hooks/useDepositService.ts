cd78e50baff57952bc32a010092e43a03ee0c6d2
jose clavo tafur <jclavotafur@gmail.com>
2021-10-07 15:15:36 -0300

    correct api models path

--- src/hooks/useDepositService.ts
+++ src/hooks/useDepositService.ts
@@ -7,14 +7,14 @@ import { Deposit } from "../interfaces/Deposit";
 import { useUtils } from "../hooks/useUtils";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const MODEL = "deposits/"
+const MODEL = "deposits"
 
 export function useDepositService() {
 
     const { getHeader } = useUtils();
 
     const depositPagination = async (deposit : Deposit | undefined) => {
-        const URL = API_URL + MODEL + 'pagination';
+        const URL = API_URL + MODEL + '/' + 'pagination';
 
         const res = await axios.post(URL, deposit, getHeader());
         return res.data as unknown as Response;
@@ -28,14 +28,14 @@ export function useDepositService() {
     };
 
     const depositGetById = async (id : number | undefined) => {
-        const URL = API_URL + MODEL + id;
+        const URL = API_URL + MODEL + '/' + id;
 
         const res = await axios.get(URL, getHeader());
         return res.data as unknown as Response;
     };
 
     const depositUpdateStatus = async (deposit : Deposit | undefined) => {
-        const URL = API_URL + MODEL + 'update-status';
+        const URL = API_URL + MODEL + '/' + 'update-status';
 
         const res = await axios.post(URL, deposit, getHeader());
         return res.data as unknown as Response;
--- src/hooks/usePurchaseService.ts
+++ src/hooks/usePurchaseService.ts
@@ -7,14 +7,14 @@ import { Purchase } from "../interfaces/Purchase";
 import { useUtils } from "../hooks/useUtils";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const MODEL = "purchases/"
+const MODEL = "purchases"
 
 export function usePurchaseService() {
 
     const { getHeader } = useUtils();
 
     const purchasePagination = async (purchase : Purchase | undefined) => {
-        const URL = API_URL + MODEL + 'pagination';
+        const URL = API_URL + MODEL + '/' + 'pagination';
 
         const res = await axios.post(URL, purchase, getHeader());
         return res.data as unknown as Response;
